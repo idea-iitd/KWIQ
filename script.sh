64d2a439c7906ded15ea771fb4e693808c20453b
Mahdihusain Momin <mahdihusainmomin@gmail.com>
2019-05-06 01:11:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf *.class
 javac Kwin.java
-java Kwin DatasetFilePath startTime queryWindow threshold
+# java Kwin DatasetFilePath startTime queryWindow threshold
+java Kwin 15wiki.txt 1078412216 1 7
